13d5701dcd3ad9bc0c5a554fbb8bf0ea9eec625d
venegasjosh <venegasjosh@gmail.com>
2020-06-19 13:06:08 -0700

    installed header

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.componenet";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 
 function App() {
   return (
     <div>
+    <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
