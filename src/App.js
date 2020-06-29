943574b817dd688c670500a09c48dbf261032a7b
venegasjosh <venegasjosh@gmail.com>
2020-06-28 20:14:49 -0700

    made button and form component to add to sign-in and sign-in-and-sign-up component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Switch, Route } from 'react-router-dom';
 import "./App.css";
 
-import HomePage from "./pages/homepage/homepage.componenet";
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import Header from './components/header/header.component';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './custom-button.styles.scss'
+
+const CustomButton = ({children, ...otherProps}) => (
+
+<button className='custom-button' {...otherProps}>
+{children}
+
+</button>
+
+
+
+)
+
+export default CustomButton
